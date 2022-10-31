00c6d141718eedf62070b17a38e6dd9c304f1c1e
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-10-31 19:33:29 +0900

    f: rtan + [i]로 파일 이름으로 랜덤하게 불러오기

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        int[] images = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        // search about it later
+        images = images.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
         for(int i = 0; i < 16; i++)
         {
             GameObject newCard = Instantiate(card);
@@ -20,6 +25,9 @@ public class GameManager : MonoBehaviour
             float posX = (i / 4) * 1.4f - 2.1f;
             float posY = (i % 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(posX, posY, 0);
+
+            string rtanName = "rtan" + images[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(rtanName);
         }
     }
 
